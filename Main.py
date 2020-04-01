611122ca22db3924ba6743fdf7065cf99e583843
priota21 <priota35-1481@diu.edu.bd>
2020-04-02 01:02:10 +0600

    USER SIGNUP ADDED

--- Main.py
+++ Main.py
@@ -1,19 +1,48 @@
-from flask import Flask
-from flask import Response, request
-import pymongo
 import json
 
-app= Flask(__name__)
-# this is the DB connection portion
+import pymongo
+from flask import Flask
+from flask import Response, request
+
+app = Flask(__name__)
 try:
     mongo = pymongo.MongoClient(
-       host= "localhost" ,
-        port= 27017,
-        serverSelectionTimeoutMS= 1000
+        host="localhost",
+        port=27017,
+        serverSelectionTimeoutMS=1000
     )
-    db= mongo.bookslibrary
+    db = mongo.bookslibrary
     mongo.server_info()
 except:
     print("cannot connect to DB")
 
+
+###########################################
+@app.route('/user/signUp', methods=['POST'])
+def user_signup():
+    try:
+        userDetails = request.get_json()
+        dbResponse = db.users.insert_one(userDetails)
+
+        return Response(
+            response=json.dumps(
+                {"user": "User created Successfully",
+                 "userid": f"{dbResponse.inserted_id}"
+                 }
+            ),
+            status=201,
+            mimetype='application/json'
+        )
+
+    except Exception as ex:
+        print('*********************')
+
+        print("ex")
+        print('********************')
+
+
+###########################################
+
+
+if __name__ == "__main__":
     app.run()
